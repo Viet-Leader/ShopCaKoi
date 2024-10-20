66f14ea789103f58e96983aeaf91c27e73bccf44
Viet-Leader <viettq2511@ut.edu.vn>
2024-10-20 20:11:43 +0700

    files goc

---
+++ PremierLeague/PremierLeague.Repositories/Entities/FootballPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PremierLeague.Repositories.Entities;
+
+public partial class FootballPlayer
+{
+    public string FootballPlayerId { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Achievements { get; set; } = null!;
+
+    public DateTime? Birthday { get; set; }
+
+    public string PlayerExperiences { get; set; } = null!;
+
+    public string Nomination { get; set; } = null!;
+
+    public string? FootballClubId { get; set; }
+
+    public virtual FootballClub? FootballClub { get; set; }
+}
---
+++ PremierLeague/PremierLeague.Sevices/PremierLeagueAccountSevices.cs
@@ -0,0 +1,25 @@
+﻿using PremierLeague.Repositories;
+using PremierLeague.Repositories.Entities;
+using PremierLeague.Sevices.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierLeague.Sevices
+{
+    public class PremierLeagueAccountSevices : IPremierLeagueAccountSevices
+    {
+        private readonly PremierLeagueAccountRepositories _repository;
+        public PremierLeagueAccountSevices(PremierLeagueAccountRepositories _repository) 
+        {
+            this._repository = _repository;
+        }
+
+        public Task<List<PremierLeagueAccount>> PremierLeagueAccounts()
+        {
+            return _repository.GetAllPremierLeagueAccounts();
+        }
+    }
+}
